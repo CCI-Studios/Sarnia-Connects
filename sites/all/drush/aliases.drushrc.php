802a1736781d8ae3b22cca3ad83773d2ef445a56
Chris Morris <cmorris@ccistudios.com>
2018-02-09 12:06:13 -0500

    added drush alias for live site

--- sites/all/drush/aliases.drushrc.php
+++ sites/all/drush/aliases.drushrc.php
@@ -10,3 +10,13 @@ $aliases['dev'] = array(
 	),
 	'ssh-options'=>'-p 37241'
 );
+
+$aliases['live'] = array(
+	'uri'=> 'sarniaconnects.com',
+	'root' => '/home/sarniaconnects/subdomains/live/public_html',
+	'remote-host'=> 'host.cciserver2.com',
+	'remote-user'=> 'sarniaconnects',
+	'path-aliases'=> array(
+		'%files'=> 'sites/default/files',
+	)
+);
